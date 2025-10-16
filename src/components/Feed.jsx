fcc55f61f0c3ae23ca6db52185db93553ce6ad4a
HarshikaSaini <harshikasaini1415@gmail.com>
2025-10-16 23:51:16 +0530

    added skills design

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -7,16 +7,23 @@ const Feed = () => {
           alt="Shoes"
         />
       </figure>
-      <div className="card-body">
+      <div className="card-body bg-gray-700">
         <h2 className="card-title">
           Abhishek sharma
           <div className="badge badge-secondary">Male</div>
         </h2>
         <p>
           A card component has a figure, a body part, and inside body there are
-          title and actions parts  A card component has a figure, a body part, and inside body there are
-          title and actions parts
+          title and actions parts A card component has a figure, a body part,
+          and inside body there are title and actions parts
         </p>
+        <div className="flex flex-row items-center justify-start gap-2 my-4">
+         <div className="badge badge-primary ">Front End</div>
+         <div className="badge badge-primary ">Back End</div>
+         <div className="badge badge-primary ">LLM's</div>
+         <div className="badge badge-primary ">Web Development</div>
+         <div className="badge badge-primary ">Devops</div>
+         </div>
         <div className="card-actions justify-end">
           <div className="badge badge-outline">Fashion</div>
           <div className="badge badge-outline">Products</div>
