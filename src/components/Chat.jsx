814d962b541342ac3be44b8c39fdfec1c053529d
HarshikaSaini <harshikasaini1415@gmail.com>
2025-11-05 23:41:51 +0530

    Added get api for fetching chat and updated state

--- src/components/Chat.jsx
+++ src/components/Chat.jsx
@@ -2,13 +2,14 @@ import { useEffect, useState } from "react";
 import { createSocketConnection } from "../redux/socket";
 import { useParams } from "react-router-dom";
 import { useSelector } from "react-redux";
+import axios from "axios";
 
 const Chats = () => {
   const [mess, setMess] = useState("");
   const [messList, setMessList] = useState([]);
   const { targeted_user_id } = useParams();
   const user = useSelector((store) => store.user);
-  const { firstName, _id } = user?.data || {};
+  const { firstName,lastName,photoUrl ,_id} = user?.data || {};
   const socket = createSocketConnection();
 
   useEffect(() => {
@@ -16,77 +17,105 @@ const Chats = () => {
       return;
     }
     socket.emit("joinchat", { firstName, _id, targeted_user_id });
-    socket.on("messRecieved", ({ firstName, mess, userID }) => {
-      setMessList((prev) => [...prev, { firstName, mess, userID }]);
+    socket.on("messRecieved", ({ firstName,lastName,photoUrl,mess, userID, createdAt }) => {
+      setMessList((prev) => [...prev, {firstName,lastName,photoUrl,mess, userID,createdAt}]);
     });
     return () => {
       socket.disconnect();
     };
   }, [_id, targeted_user_id]);
+ 
+  const fetchChat = async()=>{
+   try {
+    const res = await axios.get(`${import.meta.env.VITE_BASE_URL}/chat/${targeted_user_id}`, {withCredentials:true})
+    const messData = res?.data.message.map(msg => {
+      return{
+        firstName:msg.senderId.firstName,
+        lastName:msg.senderId.lastName,
+        photoUrl:msg.senderId.photoUrl,
+        mess:msg.text,
+        userID:msg.senderId._id,
+        createdAt:msg.createdAt
+      }
+    })
+    setMessList(messData)
+   } catch (error) {
+    console.log("error in fetching chat",error)
+   }
+  }
 
-  const handleEnter = () =>{
-    
+  useEffect(()=>{
+    fetchChat()
+  },[])
+ 
+  const handleEnter = (e) =>{
+    if(e.key === "Enter"){
+      sendNewMessage()
+    }
   }
 
   const sendNewMessage = () => {
     socket.emit("sendMessage", {
       firstName,
+      lastName,
+      photoUrl,
       userID: _id,
       targetID: targeted_user_id,
       mess,
+      createdAt:new Date()
     });
     setMess("");
   };
 
-  const getCUrrentTime = () => {
-    const now = new Date();
+  const getCUrrentTime = (currTime) => {
+    const now = new Date(currTime);
     const time = now.toLocaleTimeString([], {
       hour: "2-digit",
       minute: "2-digit",
-      second: "2-digit",
-      hour12: false,
+      // second: "2-digit",
+      hour12: true,
     });
     return time
   }
- 
+
   return (
     <div className="border-2 rounded-md border-gray-700 h-full w-1/2 m-auto flex flex-col p-2">
       <div className="flex-1 overflow-y-auto mb-5">
-        {messList.map((item) => {
+        {messList.length > 0 && messList.map((item,idx) => {
           return (
             <>
-              {item.userID === _id ? (
-                <div className="chat chat-start">
+              {item?.userID === _id ? (
+                <div key={idx} className="chat chat-start">
                   <div className="chat-image avatar">
                     <div className="w-10 rounded-full">
                       <img
                         alt="Tailwind CSS chat bubble component"
-                        src="https://img.daisyui.com/images/profile/demo/kenobee@192.webp"
+                        src={item?.photoUrl}
                       />
                     </div>
                   </div>
                   <div className="chat-header">
-                    {item.firstName}
-                    <time className="text-xs opacity-50">{getCUrrentTime()}</time>
+                    {`${item?.firstName}  ${item?.lastName}`}
+                    <time className="text-xs opacity-50">{getCUrrentTime(item?.createdAt)}</time>
                   </div>
-                  <div className="chat-bubble">{item.mess}</div>
+                  <div className="chat-bubble">{item?.mess}</div>
                   {/* <div className="chat-footer opacity-50">Delivered</div> */}
                 </div>
               ) : (
-                <div className="chat chat-end">
+                <div key={idx} className="chat chat-end">
                   <div className="chat-image avatar">
                     <div className="w-10 rounded-full">
                       <img
                         alt="Tailwind CSS chat bubble component"
-                        src="https://img.daisyui.com/images/profile/demo/anakeen@192.webp"
+                        src={item?.photoUrl}
                       />
                     </div>
                   </div>
                   <div className="chat-header">
-                    {item.firstName}
-                    <time className="text-xs opacity-50">{getCUrrentTime()}</time>
+                    {`${item?.firstName}  ${item?.lastName}`}
+                    <time className="text-xs opacity-50">{getCUrrentTime(item?.createdAt)}</time>
                   </div>
-                  <div className="chat-bubble">{item.mess}</div>
+                  <div className="chat-bubble">{item?.mess}</div>
                   {/* <div className="chat-footer opacity-50">Seen at 12:46</div> */}
                 </div>
               )}
@@ -104,7 +133,7 @@ const Chats = () => {
           onChange={(e) => setMess(e.target.value)}
           onKeyDown={handleEnter}
         />
-        <button onClick={sendNewMessage} className="btn btn-primary">
+        <button onClick={(e) => sendNewMessage(e)} className="btn btn-primary">
           Send
         </button>
       </div>
