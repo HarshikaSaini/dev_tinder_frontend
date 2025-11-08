20a6dc7f819bf2dc2edba2e331c9364b8257b1fa
HarshikaSaini <harshikasaini1415@gmail.com>
2025-11-08 22:52:48 +0530

    Added other emoji,attachment,etc inputs

--- src/components/Chat.jsx
+++ src/components/Chat.jsx
@@ -1,58 +1,72 @@
-import { useEffect, useState,useRef } from "react";
+import { useEffect, useState, useRef } from "react";
 import { createSocketConnection } from "../redux/socket";
 import { useParams } from "react-router-dom";
 import { useSelector } from "react-redux";
 import axios from "axios";
+import { MdOutlineEmojiEmotions } from "react-icons/md";
+import EmojiPicker from "emoji-picker-react";
 
+import { IoAttachOutline, IoCameraOutline, IoSend } from "react-icons/io5";
 const Chats = () => {
   const [mess, setMess] = useState("");
   const [messList, setMessList] = useState([]);
   const { targeted_user_id } = useParams();
   const user = useSelector((store) => store.user);
-  const { firstName,lastName,photoUrl ,_id} = user?.data || {};
+  const { firstName, lastName, photoUrl, _id } = user?.data || {};
   const socket = createSocketConnection();
-
+  const messagesContainerRef = useRef(null);
+  const [showEmojiPicker, setShowEmojiPicker] = useState(false);
+ 
   useEffect(() => {
     if (!_id && !firstName) {
       return;
     }
     socket.emit("joinchat", { firstName, _id, targeted_user_id });
-    socket.on("messRecieved", ({ firstName,lastName,photoUrl,mess, userID, createdAt }) => {
-      setMessList((prev) => [...prev, {firstName,lastName,photoUrl,mess, userID,createdAt}]);
-    });
+    socket.on(
+      "messRecieved",
+      ({ firstName, lastName, photoUrl, mess, userID, createdAt }) => {
+        setMessList((prev) => [
+          ...prev,
+          { firstName, lastName, photoUrl, mess, userID, createdAt },
+        ]);
+      }
+    );
     return () => {
       socket.disconnect();
     };
   }, [_id, targeted_user_id]);
- 
-  const fetchChat = async()=>{
-   try {
-    const res = await axios.get(`${import.meta.env.VITE_BASE_URL}/chat/${targeted_user_id}`, {withCredentials:true})
-    const messData = res?.data.message.map(msg => {
-      return{
-        firstName:msg.senderId.firstName,
-        lastName:msg.senderId.lastName,
-        photoUrl:msg.senderId.photoUrl,
-        mess:msg.text,
-        userID:msg.senderId._id,
-        createdAt:msg.createdAt
-      }
-    })
-    setMessList(messData)
-   } catch (error) {
-    console.log("error in fetching chat",error)
-   }
-  }
 
-  useEffect(()=>{
-    fetchChat()
-  },[])
- 
-  const handleEnter = (e) =>{
-    if(e.key === "Enter"){
-      sendNewMessage()
+  const fetchChat = async () => {
+    try {
+      const res = await axios.get(
+        `${import.meta.env.VITE_BASE_URL}/chat/${targeted_user_id}`,
+        { withCredentials: true }
+      );
+      const messData = res?.data.message.map((msg) => {
+        return {
+          firstName: msg.senderId.firstName,
+          lastName: msg.senderId.lastName,
+          photoUrl: msg.senderId.photoUrl,
+          mess: msg.text,
+          userID: msg.senderId._id,
+          createdAt: msg.createdAt,
+        };
+      });
+      setMessList(messData);
+    } catch (error) {
+      console.log("error in fetching chat", error);
     }
-  }
+  };
+
+  useEffect(() => {
+    fetchChat();
+  }, []);
+
+  const handleEnter = (e) => {
+    if (e.key === "Enter") {
+      sendNewMessage();
+    }
+  };
 
   const sendNewMessage = () => {
     socket.emit("sendMessage", {
@@ -62,9 +76,10 @@ const Chats = () => {
       userID: _id,
       targetID: targeted_user_id,
       mess,
-      createdAt:new Date()
+      createdAt: new Date(),
     });
     setMess("");
+    setShowEmojiPicker(false)
   };
 
   const getCUrrentTime = (currTime) => {
@@ -75,73 +90,101 @@ const Chats = () => {
       // second: "2-digit",
       hour12: true,
     });
-    return time
-  }
-const messagesContainerRef = useRef(null);
+    return time;
+  };
 
-useEffect(() => {
-  if (messagesContainerRef.current) {
-    messagesContainerRef.current.scrollTop = messagesContainerRef.current.scrollHeight;
+  useEffect(() => {
+    if (messagesContainerRef.current) {
+      messagesContainerRef.current.scrollTop =
+        messagesContainerRef.current.scrollHeight;
+    }
+  }, [messList]);
+
+  const handleChange = (emojiData) =>{
+     setMess((prev)=> prev + emojiData.emoji)
   }
-}, [messList]);
+
   return (
     <div className="border-2 rounded-md border-gray-700 h-full w-1/2 m-auto flex flex-col p-2">
       <div ref={messagesContainerRef} className="flex-1 overflow-y-auto mb-5">
-        {messList.length > 0 && messList.map((item,idx) => {
-          return (
-            <>
-              {item?.userID === _id ? (
-                <div key={idx} className="chat chat-start">
-                  <div className="chat-image avatar">
-                    <div className="w-10 rounded-full">
-                      <img
-                        alt="Tailwind CSS chat bubble component"
-                        src={item?.photoUrl}
-                      />
+        {messList.length > 0 &&
+          messList.map((item, idx) => {
+            return (
+              <div key={idx}>
+                {item?.userID === _id ? (
+                  <div className="chat chat-start">
+                    <div className="chat-image avatar">
+                      <div className="w-5 rounded-full">
+                        <img
+                          alt="Tailwind CSS chat bubble component"
+                          src={item?.photoUrl}
+                        />
+                      </div>
                     </div>
-                  </div>
-                  <div className="chat-header">
-                    {`${item?.firstName}  ${item?.lastName}`}
-                    <time className="text-xs opacity-50">{getCUrrentTime(item?.createdAt)}</time>
-                  </div>
-                  <div className="chat-bubble">{item?.mess}</div>
-                  {/* <div className="chat-footer opacity-50">Delivered</div> */}
-                </div>
-              ) : (
-                <div key={idx} className="chat chat-end">
-                  <div className="chat-image avatar">
-                    <div className="w-10 rounded-full">
-                      <img
-                        alt="Tailwind CSS chat bubble component"
-                        src={item?.photoUrl}
-                      />
+                    <div className="chat-header">
+                      {`${item?.firstName}  ${item?.lastName}`}
+                      <time className="text-xs opacity-50">
+                        {getCUrrentTime(item?.createdAt)}
+                      </time>
                     </div>
+                    <div className="chat-bubble flex max-w-[70%] wrap-anywhere text-sm">
+                      {item?.mess}
+                    </div>
+                    {/* <div className="chat-footer opacity-50">Delivered</div> */}
                   </div>
-                  <div className="chat-header">
-                    {`${item?.firstName}  ${item?.lastName}`}
-                    <time className="text-xs opacity-50">{getCUrrentTime(item?.createdAt)}</time>
+                ) : (
+                  <div key={idx} className="chat chat-end">
+                    <div className="chat-image avatar">
+                      <div className="w-10 rounded-full">
+                        <img
+                          alt="Tailwind CSS chat bubble component"
+                          src={item?.photoUrl}
+                        />
+                      </div>
+                    </div>
+                    <div className="chat-header">
+                      {`${item?.firstName}  ${item?.lastName}`}
+                      <time className="text-xs opacity-50">
+                        {getCUrrentTime(item?.createdAt)}
+                      </time>
+                    </div>
+                    <div className="chat-bubble bottom-0 flex max-w-[70%] wrap-anywhere text-sm">
+                      {item?.mess}
+                    </div>
+                    {/* <div className="chat-footer opacity-50">Seen at 12:46</div> */}
                   </div>
-                  <div className="chat-bubble bottom-0">{item?.mess}</div>
-                  {/* <div className="chat-footer opacity-50">Seen at 12:46</div> */}
-                </div>
-              )}
-            </>
-          );
-        })}
+                )}
+              </div>
+            );
+          })}
       </div>
 
-      <div className="flex items-center justify-between gap-2 p-2 m-auto rounded-md border-gray-700 border-2 w-[90%] mb-2">
-        <input
-          className=" h-[40px] w-full border-0 outline-none pl-1"
+      <div className="flex items-center justify-between gap-2 p-2 m-auto rounded-md border-gray-700 border-2 w-[90%] mb-2 relative">
+      {showEmojiPicker && (
+        <div  className="absolute bottom-full z-10 mb-2 w-[88%]"><EmojiPicker height="500px" width="100%" skinTonePickerLocation="false" emojiStyle="google" searchPlaceHolder="Search Emoji" suggestedEmojisMode="recent"  onEmojiClick={handleChange}/></div>
+      )}
+        <MdOutlineEmojiEmotions
+          size={30}
+          className="cursor-pointer text-gray-400 "
+          onClick={() => setShowEmojiPicker((prev) => !prev)}
+        />
+        <textarea
+          className=" max-h-[25px] w-full border-0 outline-none pl-1"
           type="text"
           name="mess"
           value={mess}
           onChange={(e) => setMess(e.target.value)}
           onKeyDown={handleEnter}
         />
-        <button onClick={(e) => sendNewMessage(e)} className="btn btn-primary">
-          Send
-        </button>
+        <div className="flex gap-1">
+          <IoCameraOutline size={20} className="cursor-pointer text-gray-400" />
+          <IoAttachOutline size={20} className="cursor-pointer text-gray-400" />
+          <IoSend
+            size={20}
+            onClick={(e) => sendNewMessage(e)}
+            className="cursor-pointer text-gray-400"
+          />
+        </div>
       </div>
     </div>
   );
