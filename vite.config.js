9295f7cfd0ba61e7256f9a847c76aa78e36abe8e
HarshikaSaini <harshikasaini1415@gmail.com>
2025-11-18 00:25:27 +0530

    using loadEnv

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,11 @@
-import { defineConfig } from 'vite'
+import { defineConfig,loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
-export default defineConfig({
+export default defineConfig(({mode}) => {
+  const env = loadEnv(mode, process.cwd(),"")
+  return{
   plugins: [
     react(),
     tailwindcss(),
@@ -12,10 +14,10 @@ export default defineConfig({
   server:{
     proxy:{
       "/api":{
-        target:import.meta.env.VITE_BASE_URL,
+        target:env.VITE_BASE_URL,
         changeOrigin:true,
         secure:true
       }
     },
   },
-})
+}})
